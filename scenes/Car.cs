6de0876b9dc399b2769e55b99bdfc81c51170928
st1llsane <notescape262@gmail.com>
2026-01-19 22:56:18 +0300

    feat: spawn cars from both level sides

--- scenes/Car.cs
+++ scenes/Car.cs
@@ -1,12 +1,23 @@
 using Godot;
-using System;
 
 namespace Car
 {
   public partial class Car : Area2D
   {
+    public Sprite2D sprite;
     public Vector2 direction = Vector2.Left;
     public int speed = 1;
+
+    public override void _Ready()
+    {
+      sprite = GetNode<Sprite2D>("Sprite2D");
+
+      if (Position.X <= 0)
+      {
+        direction = Vector2.Right;
+        sprite.FlipH = true;
+      }
+    }
 
     public override void _Process(double delta)
     {
--- scenes/Game.cs
+++ scenes/Game.cs
@@ -6,17 +6,23 @@ namespace Game
   {
     private PackedScene _carScene;
     private Node2D _carsNode;
+    private Node2D _carStartPositionsNode;
 
     public override void _Ready()
     {
       _carScene = ResourceLoader.Load<PackedScene>("res://scenes/car.tscn");
       _carsNode = GetNode<Node2D>("Cars");
+      _carStartPositionsNode = GetNode<Node2D>("CarStartPositions");
     }
 
     private void _OnCarTimerTimeout()
     {
       GD.Print("Car spawned");
-      var car = _carScene.Instantiate();
+      var car = _carScene.Instantiate() as Car.Car;
+      var posMarker = _carStartPositionsNode.GetChildren().PickRandom() as Marker2D;
+
+      car.Position = posMarker.Position;
+
       _carsNode.AddChild(car);
     }
   }
